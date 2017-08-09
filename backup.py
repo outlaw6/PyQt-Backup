5791dd41dea32bb280af9a72c008d2273da3f321
outlaw6 <milan.damjanovic.mbs@gmail.com>
2017-08-09 16:29:13 +0200

    Pre-Final version

--- backup.py
+++ backup.py
@@ -8,7 +8,7 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
-
+import subprocess
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
 except AttributeError:
@@ -25,12 +25,12 @@ except AttributeError:
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
-        MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(686, 192)
+        MainWindow.setObjectName(_fromUtf8("Backup particija dd"))
+        MainWindow.resize(986, 192)
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         self.label = QtGui.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(300, 30, 321, 18))
+        self.label.setGeometry(QtCore.QRect(400, 30, 321, 18))
         font = QtGui.QFont()
         font.setBold(True)
         font.setWeight(75)
@@ -38,21 +38,22 @@ class Ui_MainWindow(object):
         self.label.setMargin(-1)
         self.label.setObjectName(_fromUtf8("label"))
         self.label_2 = QtGui.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(300, 90, 281, 18))
+        self.label_2.setGeometry(QtCore.QRect(400, 90, 281, 18))
         font = QtGui.QFont()
         font.setBold(True)
         font.setWeight(75)
         self.label_2.setFont(font)
         self.label_2.setObjectName(_fromUtf8("label_2"))
         self.lineEdit = QtGui.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(60, 30, 221, 30))
+        self.lineEdit.setGeometry(QtCore.QRect(60, 30, 300, 30))
         self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
         self.lineEdit_2 = QtGui.QLineEdit(self.centralwidget)
-        self.lineEdit_2.setGeometry(QtCore.QRect(60, 80, 221, 30))
+        self.lineEdit_2.setGeometry(QtCore.QRect(60, 80, 300, 30))
         self.lineEdit_2.setObjectName(_fromUtf8("lineEdit_2"))
         self.pushButton = QtGui.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(60, 140, 571, 28))
+        self.pushButton.setGeometry(QtCore.QRect(60, 140, 771, 28))
         self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.pushButton.clicked.connect(self.copy)
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtGui.QStatusBar(MainWindow)
         self.statusbar.setObjectName(_fromUtf8("statusbar"))
@@ -62,11 +63,22 @@ class Ui_MainWindow(object):
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
-        self.label.setText(_translate("MainWindow", "Unesite particiju koju zelite da kopirate", None))
-        self.label_2.setText(_translate("MainWindow", "Unesite destinaciju (full path)", None))
-        self.pushButton.setText(_translate("MainWindow", "KOPIRAJ", None))
+        MainWindow.setWindowTitle(_translate("KOPIRANJE PARTICIJA", "KOPIRANJE PARTICIJA", None))
+        self.label.setText(_translate("KOPIRANJE PARTICIJA", "Unesite particiju koju zelite da kopirate", None))
+        self.label_2.setText(_translate("KOPIRANJE PARTICIJA", "Unesite destinaciju (full path)", None))
+        self.pushButton.setText(_translate("KOPIRANJE PARTICIJA", "KOPIRAJ", None))
 
+
+    def copy(self):
+
+        ''' Uzima inpute iz prve i druge linije '''
+        particija = self.lineEdit.text()
+        path_to_copy = self.lineEdit_2.text()
+
+        print particija, path_to_copy
+
+        subprocess.call(["dd if=" + particija +" " + "of=" + path_to_copy], shell=True)
+        
 
 if __name__ == "__main__":
     import sys
